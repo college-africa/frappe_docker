861365b9d14f9018f630ee2e9f4f1270f1801550
Desmond Kyeremeh <elDekyfin@gmail.com>
2025-04-14 10:14:43 +0000

    Custom build

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export APPS_JSON_BASE64=$(base64 -w 0 apps.json)
+echo $APPS_JSON_BASE64
+
+docker build \
+  --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
+  --build-arg=FRAPPE_BRANCH=version-15 \
+  --build-arg=PYTHON_VERSION=3.11.6 \
+  --build-arg=NODE_VERSION=18.18.2 \
+  --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
+  --tag=first_image \
+  --file=images/custom/Containerfile .
